380e5ff8a0ea7bcbf4fd8a4f99cb3f8df961f65f
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2020-06-29 00:28:50 +0200

    Port to MelonLoader v0.2.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using MelonLoader;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("FreeLookInCars")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,24 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© 2018 Wulf Marius")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bc320585-8c10-4ece-99a4-1c57d170e828")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.1")]
-[assembly: AssemblyFileVersion("3.0.1")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
+[assembly: MelonModInfo(typeof(FreeLookInCars.Implementation), "FreeLookInCars", "3.0.1", "WulfMarius, zeobviouslyfakeacc")]
+[assembly: MelonModGame("Hinterland", "TheLongDark")]
